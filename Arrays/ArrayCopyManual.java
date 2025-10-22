a9c2f526ecc22bc8d760a92c76b0646309e4dad1
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2025-10-22 10:34:17 +0530

    Length of an array

---
+++ Arrays/ArrayCopyManual.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ArrayCopyManual {
+    public static void main(String[] args){
+        int[] arr1 = {1, 2, 3, 4, 5};
+    int[] arr2 = new int[arr1.length];
+
+    for(int i=0; i<arr1.length; i++){
+        arr2[i] = arr1[i];
+    }
+
+    System.out.println("Elements of copied array:");
+    for(int num : arr2){   
+        System.out.print(num + " ");
+    }   
+
+    }
+}
