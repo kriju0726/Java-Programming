895d11c03437a853df0c95921d8c6a94c7bbd4cf
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2025-10-29 20:52:27 +0530

    Dynamic Size Array

--- Arrays/OneDArrayList.java
+++ Arrays/OneDArrayList.java
@@ -1,39 +1,6 @@
 import java.util.*;
 
-class OneDArray {
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5};
-
-        System.out.println("Fixed Size...");
-        for (int i=0; i<nums.length; i++) {  System.out.print(nums[i] + " ");  }
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("\n\nNow.. Dynamic Size Array...");
-        System.out.print("Enter new array size: ");
-        int size = sc.nextInt();
-
-        int[] pnums = new int[size];
-
-        System.out.printf("Please!! Enter %d elements:\n", size);
-        for (int i = 0; i < size; i++) {   pnums[i] = sc.nextInt();    }
-
-        System.out.print("Your entered elements are: ");
-        for (int i = 0; i < pnums.length; i++) {
-            System.out.print(pnums[i] + " ");
-        }
-
-        sc.close(); // ✅ good practice
-    }
-}
-
-
-
-//Another Approach using Collections(or nCopies)
-
-import java.util.*;
-
-class Main {
+class OneDArrayList {
     public static void main(String[] args) {
         System.out.print("We are playing with ArrayList in java., like Vector in Cpp\n");
         
