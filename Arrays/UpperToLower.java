a278b58f9bbc90bf23b80bd6bb29f3d3d5ecdb7f
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2025-11-16 23:32:39 +0530

    Upper To Lower

---
+++ Arrays/UpperToLower.java
@@ -0,0 +1,12 @@
+
+
+public class UpperToLower {
+    public static void main(String[] args) {
+        String str = "HELLO WORLD";
+        
+        // Convert to lowercase
+        String lower = str.toLowerCase();
+
+        System.out.println("Lowercase: " + lower);
+    }
+}
